b87d2f2dbfecca9145084baa778394956c3f3804
ceyhun <ceyhunerturk1@gmail.com>
2023-11-19 00:46:25 +0300

    library improvements & process args

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,9 +1,11 @@
 #include "peachos.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 int main(int argc, char** argv)
 {
-    printf("My age is %i\n", 98);
+    print(argv[0]);
+    print("did this work?\n");
     while(1) 
     {
     }
