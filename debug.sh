8303c46c25826878775b3f48a03fdb80694b9958
ceyhun <ceyhunerturk1@gmail.com>
2023-11-20 22:25:23 +0300

    renamings

--- debug.sh
+++ debug.sh
@@ -2,4 +2,4 @@
 
 # Refresh page if gdb connection lost!
 # Try different port if not connected!
-gdbgui -p 5000 -g "gdb -ex 'add-symbol-file ./build/kernelfull.o 0x100000' -ex 'target remote | qemu-system-i386 -hda ./bin/os.bin -gdb stdio -S'"
+gdbgui -p 5000 -g "gdb -ex 'add-symbol-file ./build/kernelfull.o 0x100000' -ex 'target remote | qemu-system-i386 -hda ./bin/os.bin -gdb stdio -S' -ex 'b kernel_main' -ex 'c'"
